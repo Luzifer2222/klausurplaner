5b3542b91791e88c1fda139a8ce75264bafde7ad
dt <daniel.thielking@gmail.com>
2015-04-01 17:29:04 +0200

    Fertig Programmier. Fehler versucht abzufangen. Formulare neu gestaltet.

--- admin/abteilunganlegen.php
+++ admin/abteilunganlegen.php
@@ -2,81 +2,128 @@
 <html>
 <head>
 <title>Class Test Scheduler 'CTS' by Daniel Thielking, Robin Gebhardt, Pascal Lawitzky</title>
-<?php
-include '../config/database.conf.php';
-?>
-
-<link href="../style/style.css" rel="stylesheet" type="text/css" />
-
-<?php 
-
-$con = mysqli_connect($database_conf['host'],$database_conf['user'],$database_conf['password'],$database_conf['database']);
-
-if(isset($_POST['abtanlegen']))
-{
-  
-  $query = "insert into abteilung (name) values ('". $_POST['abtname']."');";
-  
-  mysqli_query($con, $query);
-  
-  if (mysqli_affected_rows($con) > 0 ) {
-      echo "Es wurde 1 Datensatz erzeugt";
-  }
-  else { 
-      echo "Es ist ein Fehler aufgetreten <br />Es wurde kein Datensatz erzeugt.";
-  }  
+  <!-- Angaben zum Zeichensatz, immer UTF-8 -->
+    <meta charset="utf-8"> <!-- Angabe des Zeichensatzes für HTML5 -->
+    <meta http-eqiv="content-type" content="text/html; charset=utf-8"> <!-- Angabe des Zeichensatzes für ältere Browser -->
     
-}
-elseif (isset($_POST['abtloeschen']))
-{
-    $query = "delete from abteilung where abteilungID = ".$_POST['id'].";";
-    mysqli_query($con, $query);
-    if (mysqli_affected_rows($con)>0) {
-        echo "Es wurde der Datensatz mit der ID: ".$_POST['id']. " gelöscht.";
+    <!-- Angabe wie die Seite sich verhalten soll -->
+    <meta http-equiv="expires" content="0"> <!-- Seite wird immer von Server geholt und nicht aus dem cache geladen -->
+        
+    <!-- Angabe zu den Autoren der Website und andere Informationen -->
+    <meta name="author" content="Daniel Thielking, Robin Gebhardt, Pascal Lawitzky">
+    <meta name="project-group" content="PHPmeetsSQL G3">
+    <meta name="description" content="Klausurplaner Software">
+    
+    <!-- Einfügen des FavoritenIcons -->
+    <link rel="icon" href="../pictures/favicon/favicon.png" sizes="16x16 32x32" type="images/png">
+    <!-- Einfügen der CSS-Dateine -->
+    <link href="../style/style.css" rel="stylesheet" type="text/css">
+    
+<?php
+  //Einfügen des Konfigurations skripts
+  include '../config/database.conf.php';
+ 
+  //Öffnen der Datenbankverbindung
+  $link = mysqli_connect($database_conf['host'],$database_conf['user'],$database_conf['password'],$database_conf['database']);
+
+  //Überprüfung ob der Button zum Anlegen einer neuen Abteilung betätigt wurde
+  if(isset($_POST['abtanlegen']))
+  {
+    if ($_POST['abtname'] != "")
+    {
+      //Erstellen der Abfrage zum erzeugen einer neuen Abteilung
+      //und Ausführen der Abfrage
+      $abfrageabteilung = "insert into abteilung (name) values ('". $_POST['abtname']."');";
+      mysqli_query($link, $abfrageabteilung);
+  
+      //Überprüfung ob die Abfrage erfolgreich war
+      if (mysqli_affected_rows($link) > 0 ) {
+        //Speichern der Erfolgreichen Ausgabe in der Variable
+        $ausgabe = "<p class=\"erfolgreich\">Es wurde 1 Datensatz angelegt.</p>";
+      }
+      else 
+      { 
+        //Wenn nicht erfolgreich speichern der Errormeldung in der Variablen
+        $ausgabe = "<p class=\"error\">Es ist ein Fehler aufgetreten <br />Es wurde kein Datensatz erzeugt.</p>";
+      }  
+    }
+    else //Wenn kein Abteilungsname angegeben wurde
+    {
+        //Wenn nicht erfolgreich speichern der Errormeldung in der Variablen
+        $ausgabe = "<p class=\"error\">Sie müssen einen Abteilungsnamen angeben.</p>";
+    }  
+  }
+  
+  //Wenn der Button zum Löschen einer Abteilung gedrückt wurde
+  if (isset($_POST['loescheabt']) && isset($_POST['loesche']))
+  {
+    //Speichern der Abfrage zum löschen einer Abteilung
+    //und Ausführen der Abfrage  
+    $abfrageabteilung = "delete from abteilung where abteilungID = ".$_POST['loesche'].";";
+    mysqli_query($link, $abfrageabteilung);
+    
+    //Überprüfung ob die Abfrage erfolgreich war
+    if (mysqli_affected_rows($link)>0) {
+      //Speichern der Erfolgsmeldung in der Variable
+      $ausgabe = "<p class=\"erfolgreich\">Es wurde der Datensatz mit der ID: ".$_POST['loesche']. " gelöscht.</p>";
     }
     else 
     {
-        echo "Fehler! es wurde kein Datensatz gelöscht";    
+      //Wenn die Abteilung nicht gelöscht wurde
+      //Speichern der Error meldung in die Variable
+      $ausgabe = "<p class=\"error\">Fehler! es wurde kein Datensatz gelöscht</p>";    
     }
-}
-$datenbankAusgabe = mysqli_query($con, "select * from abteilung");
+  }
+ 
+  
+  //Abfrage zur Ausgabe der Tabelle Abteilung
+  //zur Übersicht 
+  $datenbankAusgabe = mysqli_query($link, "select * from abteilung");
 
-mysqli_close($con);
 ?>
 </head>
 <body>
-<table border="1">
-  <tr>
-    <td>
-      <h1>Abteilung anlegen</h1>
-      <form action="<?php $_SERVER['PHP_SELF']?>" method="post">
-      <p>Abteilung<input type="text" min="4" maxlength="50" name="abtname" /></p>
-      <p><input type="submit" name="abtanlegen" value="Abteilung Anlegen" /></p>
-      </form>
-    </td>
-    <td>
-      <h1>Abteilung löschen</h1>
-      <form action="<?php $_SERVER['PHP_SELF']?>" method="post">
-      <p>Angabe der ID <input type="number" name="id" /></p>
-      <p><input type="submit" name="abtloeschen" value="Löschen" /></p>
-      </form>
-    </td>
-  </tr>
-</table>
-<br /><br /><br />
-<table>
-  <tr>
-    <td>AbteilungsID</td>
-    <td>Abteilungsname</td>
-   <?php 
-   while($ausgabe = mysqli_fetch_assoc($datenbankAusgabe))
-   {
-   echo "<tr>";
-   echo "<td>".$ausgabe["abteilungID"]."</td>"."<td>".$ausgabe["name"]."</td>";
-   echo "</tr>";
-   }
-   ?>
-</table>
-</body>
 
+  <!-- Formular zum erstellen einer neuen Abteilung -->
+  <form action="<?php $_SERVER['PHP_SELF']?>" method="post">
+    <fieldset>
+      <legend>Abteilung anlegen</legend>
+      <p><label>Abteilung: <br><input type="text" min="4" maxlength="50" name="abtname" /></label></p>
+      <p><input type="submit" name="abtanlegen" value="Abteilung anlegen" /></p>
+      </fieldset>
+  </form>
+      
+  <?php
+  if(isset($ausgabe)) 
+  echo $ausgabe;
+  ?>
+      
+  <table>
+    <tr>
+      <th>AbteilungsID</th>
+      <th>Abteilungsname</th>
+      <th><form action="<?php $_SERVER['PHP_SELF'] ?>" method="post">
+          <input type="submit" name="loescheabt" value="Löschen?"></th>
+    </tr>
+       <?php 
+         while($lehrertabelle = mysqli_fetch_assoc($datenbankAusgabe))
+         {
+         echo "<tr>";
+         echo "<td>".$lehrertabelle["abteilungID"]."</td>";
+         echo "<td>".$lehrertabelle["name"]."</td>";
+         echo "<td><input type=\"radio\" name=loesche value=\"".$lehrertabelle["abteilungID"]."\">";
+         echo "</tr>";
+       }
+       ?>
+    <tr>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td><input type="submit" name="loescheabt" value="Löschen?"></form></td>
+    </tr> 
+</table>
+
+</body>
+<?php 
+mysqli_close($link); 
+?>
 </html>
