bfcf5220499f89ea5aa52d58b6689243127d2c37
dt <daniel.thielking@gmail.com>
2015-04-10 11:38:26 +0200

    Erweitern der Methode berechneFeiertag um alle Feiertage der Bundesrepublik

--- include/kalender.class.php
+++ include/kalender.class.php
@@ -18,6 +18,8 @@ class kalender
 	private $fach;
 
 	private $lehrer;
+	
+	private $bundesland;
 
 	function baueKalender ($jahr, $monat)
 	{
@@ -76,7 +78,7 @@ class kalender
 		}
 		
 		// Beginn der Kalender Tabelle
-		echo "<table border=1>";
+		echo "<table class=\"kalender\">";
 		
 		/*
 		 * Schleife für die Zeilenanzahl Anzahl der Zeilen sind 7
@@ -104,14 +106,14 @@ class kalender
 				{
 					if ($ersterWochentagImMonat == $spalten && $wurdeErsterWochentagGeschrieben == false)
 					{
-						echo "<td>$tagDatum</td>";
+						echo "<td><p class=\"datum\">$tagDatum</p></td>";
 						$tagDatum++;
 						$beginneMitNummerierung = true;
 						$wurdeErsterWochentagGeschrieben = true;
 					}
 					elseif ($beginneMitNummerierung == true && $tagDatum <= $anzahlTageDesMonats)
 					{
-						echo "<td>$tagDatum</td>";
+						echo "<td><p class=\"datum\">$tagDatum</p></td>";
 						$tagDatum++;
 					}
 					else
@@ -124,39 +126,35 @@ class kalender
 		echo "</table>";
 	}
 
-	function berechneFeiertage ($jahr)
+	function berechneFeiertage ($jahr, $bundesland)
 	{
 		// Berechnung der Variablen Feiertage anhand der easter_date Funktion
 		$osterSonntag = date("d.m.Y", easter_date($jahr));
 		$osterMontag = date("d.m.Y", strtotime("$osterSonntag +1 day"));
 		$karFreitag = date("d.m.Y", strtotime("$osterSonntag -2 day"));
-		$ascherMittwoch = date("d.m.Y", strtotime("$osterSonntag -46 day"));
-		$veilchenDienstag = date("d.m.Y", strtotime("$ascherMittwoch -1 day"));
-		$rosenMontag = date("d.m.Y", strtotime("$ascherMittwoch -2 day"));
-		$weiberFastnacht = date("d.m.Y", strtotime("$ascherMittwoch -6 day"));
 		$christiHimmelfahrt = date("d.m.Y", strtotime("$osterSonntag +40 day"));
+		$pfingstSonntag = date("d.m.Y", strtotime("$osterSonntag +49 day"));
 		$pfingstMontag = date("d.m.Y", strtotime("$osterSonntag +50 day"));
 		$fronleichnahm = date("d.m.Y", strtotime("$osterSonntag +60 day"));
 		
 		// Feste Feiertage Nordrhein-Westfalen
 		$neujahr = date("d.m.Y", mktime(0, 0, 0, 1, 1, $jahr));
+		$heiligDreiKoenige = date("d.m.Y", mktime(0, 0, 0, 1, 6, $jahr));
 		$tagDerArbeit = date("d.m.Y", mktime(0, 0, 0, 5, 1, $jahr));
+		$mariaeHimmelfahrt = date("d.m.Y", mktime(0, 0, 0, 8, 15, $jahr));
 		$tagDerDeutschenArbeit = date("d.m.Y", mktime(0, 0, 0, 10, 3, $jahr));
+		$reformationstag = date("d.m.Y", mktime(0, 0, 0, 10, 31, $jahr));
 		$allerheiligen = date("d.m.Y", mktime(0, 0, 0, 11, 1, $jahr));
 		$ersterWeihnachtsfeiertag = date("d.m.Y", mktime(0, 0, 0, 12, 25, $jahr));
 		$zweiterWeihnachtsfeiertag = date("d.m.Y", mktime(0, 0, 0, 12, 26, $jahr));
+		
 		
 		// Rückgabe aller Feiertage mit dazugehörigem Namen
 		// Zur Ausgabe muss eine foreach ( $name as $key => $value ) verwendet werden
 		return $this->feiertage = array(
 			
 				"Neujahr" => $neujahr, 
-				"Weiberfastnacht" => $weiberFastnacht, 
-				"Rosenmontag" => $rosenMontag, 
-				"Veilchendienstag" => $veilchenDienstag, 
-				"Aschermittwoch" => $ascherMittwoch, 
 				"Karfreitag" => $karFreitag, 
-				"Ostersonntag" => $osterSonntag, 
 				"Ostermontag" => $osterMontag, 
 				"Tag der Arbeit" => $tagDerArbeit, 
 				"Christi Himmelfahrt" => $christiHimmelfahrt, 
