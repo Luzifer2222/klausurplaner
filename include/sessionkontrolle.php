3c73cd45658404cbf0c837fc3e83853ce52778b3
dt <daniel.thielking@gmail.com>
2015-04-10 09:32:58 +0200

    Erstellung der Klasse sessionkontrolle, da einfacher die Session über das Objekt zu prüfen ist

---
+++ include/sessionkontrolle.php
@@ -0,0 +1,45 @@
+<?php
+
+class sessionkontrolle
+{
+
+	function AdminBereich ()
+	{
+		// Starten/Wiederaufnehmen einer Session
+		session_start();
+		
+		// Prüfen ob das Array der der Session initialisiert wurde
+		if (!isset($_SESSION['benutzername']) && !isset($_SESSION['administrator']))
+		{
+			// die Session nicht initialisiert wurde.
+			exit("<p>Sie haben keinen Zugang zu der Seite!<br><a href=\"/login.php\">Login Seite</a></p>");
+		
+		}
+		
+		if ($_SESSION['administrator'] != true)
+		{
+			
+			// Programm abbruch, da der Benutzer kein Administrator ist
+			exit("<p>Sie haben keinen Zugang zu der Seite!<br><a href=\"/login.php\">Login Seite</a></p>");
+		}
+	
+	}
+
+	function UserBereich ()
+	{
+		// Starten/Wiederaufnehmen einer Session
+		session_start();
+		
+		// Prüfen ob das Array der der Session initialisiert wurde
+		if (!isset($_SESSION['benutzername']))
+		{
+			// Programm abbruch, da die Session nicht initialisiert wurde.
+			exit(
+						"<p>Benutzername oder Passwort falsch!</p>
+          <p>Sie haben keinen Zugang zu der Seite! <br>
+		  <a href=login.php>Login Seite</a>");
+		}
+	}
+}
+
+?>
