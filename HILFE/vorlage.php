9f981d5cce8daaa79541c4783cd20ecea62bb788
dt <daniel.thielking@gmail.com>
2015-04-10 11:38:52 +0200

    Löschen der javascrip_test.php Anpassen der vorlage.php

--- HILFE/vorlage.php
+++ HILFE/vorlage.php
@@ -1,47 +1,43 @@
 <!-- Grundgerüst für HTML-Dokumente bitte immer dieses Verwenden!  -->
 
 <?php
-// Starten/Wiederaufnehmen einer Session
-session_start();
-
-/* Für normalen Benutzerbereich */
-// Prüfen ob das Array der der Session initialisiert wurde
-if (!isset($_SESSION['benutzername']))
-{
-	// Programm abbruch, da die Session nicht initialisiert wurde.
-	exit("<p>Sie haben keinen Zugang zu der Seite!<br><a href=\"/login.php\">Login Seite</a>");
-}
+// Einfügen der Bibliotheken
+include '../include/sessionkontrolle.php';
 ?>
 
-<!doctype html>
+<?php
+
+$pruefeSession = new sessionkontrolle();
+$pruefeSession->UserBereich();
+
+?>
+
 <html>
 <head>
-<!-- Angaben zum Zeichensatz, immer UTF-8 -->
-<meta charset="utf-8">
-<!-- Angabe des Zeichensatzes für HTML5 -->
-<meta http-equiv="content-type" content="text/html; charset=utf-8">
-<!-- Angabe des Zeichensatzes für ältere Browser -->
-
-<!-- Angabe wie die Seite sich verhalten soll -->
-<meta http-equiv="expires" content="0">
-<!-- Seite wird immer von Server geholt und nicht aus dem cache geladen -->
-
-<!-- Angabe zu den Autoren der Website und andere Informationen -->
-<meta name="author"
-	content="Daniel Thielking, Robin Gebhardt, Pascal Lawitzky">
-<meta name="project-group" content="PHPmeetsSQL G3">
-<meta name="description" content="Klausurplaner Software">
-
-<!-- Einfügen des FavoritenIcons -->
-<link rel="icon" href="../pictures/favicon/favicon.png"
-	sizes="16x16 32x32" type="images/png">
-<!-- Einfügen der CSS-Dateien -->
-<link rel="stylesheet" href="Hier Stylesheet rein" type="text/css">
-
-<title>Class Test Scheduler 'CTS' by Daniel Thielking, Robin Gebhardt,
-	Pascal Lawitzky</title>
+<?php
+// Einfügen der im head-Bereich nötigen Informationen
+include '../html_include/head.php';
+?>
 </head>
 <body>
-	<!-- sichtbarer Inhalt -->
+	<div id="container">
+		<?php
+		include '../html_include/header.php';
+		include '../html_include/navigation.php';
+		?>
+		<div id="content">
+		
+			<main>
+			<!-- Hier kommt Content -->
+			 </main>
+			
+		</div>
+		
+		<?php
+		include '../html_include/footer.php';
+		?>
+
+	</div>
 </body>
+
 </html>
