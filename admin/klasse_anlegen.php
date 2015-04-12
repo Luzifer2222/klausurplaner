b955f1da9c518832d10f6b264f9781de1fe2cb79
dt <daniel.thielking@gmail.com>
2015-04-12 11:47:45 +0200

    Datei erstellt und Vorlage reinkopiert.

---
+++ admin/klasse_anlegen.php
@@ -0,0 +1,44 @@
+<!-- Grundgerüst für HTML-Dokumente bitte immer dieses Verwenden!  -->
+
+<?php
+// Einfügen der Bibliotheken
+$wurzelVerzeichnis = realpath($_SERVER['DOCUMENT_ROOT']);
+include_once $wurzelVerzeichnis.'/include/sessionkontrolle.class.php';
+?>
+
+<?php
+
+$pruefeSession = new sessionkontrolle();
+$pruefeSession->UserBereich();
+
+?>
+<!Doctype html>
+<html>
+<head>
+<?php
+// Einfügen der im head-Bereich nötigen Informationen
+include_once $wurzelVerzeichnis . '/html_include/head.php';
+?>
+</head>
+<body>
+	<div id="container">
+		<?php
+		include_once $wurzelVerzeichnis.'/html_include/header.php';
+		include_once $wurzelVerzeichnis.'/html_include/navigation.php';
+		?>
+		<div id="content">
+		
+			<main>
+			<!-- Hier kommt Content -->
+			 </main>
+			
+		</div>
+		
+		<?php
+		include_once $wurzelVerzeichnis.'/html_include/footer.php';
+		?>
+
+	</div>
+</body>
+
+</html>
