62b76fc21d69d7b3d17eeb15b2dd6abb49acd9c9
dt <daniel.thielking@gmail.com>
2015-04-17 16:19:17 +0200

    misc

--- kalender/kalender_anzeigen.php
+++ kalender/kalender_anzeigen.php
@@ -1,8 +1,8 @@
 <?php
 // Einfügen der Bibliotheken
 $wurzelVerzeichnis = realpath($_SERVER['DOCUMENT_ROOT']);
-include_once $wurzelVerzeichnis.'/include/sessionkontrolle.class.php';
-include_once $wurzelVerzeichnis.'/include/kalender.class.php';
+include_once $wurzelVerzeichnis . '/include/sessionkontrolle.class.php';
+include_once $wurzelVerzeichnis . '/include/kalender.class.php';
 ?>
 
 <?php
@@ -18,34 +18,60 @@ $pruefeSession->UserBereich();
 // Einfügen der im head-Bereich nötigen Informationen
 include '../html_include/head.php';
 ?>
+
+<?php
+
+$schuljahresbegin = date("d.m.Y", mktime(0, 0, 0, 1, 1, date("Y", time()) - 2));
+$schuljahresende = date("d.m.Y", strtotime("$schuljahresbegin +1 year"));
+
+?>
 </head>
 <body>
 	<div id="container">
 		<?php
-		include_once $wurzelVerzeichnis.'/html_include/header.php';
-		include_once $wurzelVerzeichnis.'/html_include/navigation.php';
+		include_once $wurzelVerzeichnis . '/html_include/header.php';
+		include_once $wurzelVerzeichnis . '/html_include/navigation.php';
 		?>
 		<div id="content">
-		
+
 			<main>
-			<?php 
+			<form action="<?php $_SERVER['PHP_SELF']?>" method="post">
+				<fieldset>
+					<legend>Auswahl des Schuljahrs</legend>
+					<label for="schuljahr"></label> <select id="schuljahr" name="schuljahr">
+					<?php
+					
+					for ($i = 0 ; $i < 5 ; $i++)
+					{
+						echo "<option value=\"" . date("Y", strtotime($schuljahresbegin)) . "\">" . date("Y", strtotime($schuljahresbegin)) . "/" .
+									 date("Y", strtotime($schuljahresende)) . "</option>";
+						$schuljahresbegin = date("d.m.Y", strtotime("$schuljahresbegin +1 year"));
+						$schuljahresende = date("d.m.Y", strtotime("$schuljahresende +1 year"));
+					}
+					
+					?>
+				</select> 
+				<input type="submit" name="anzeigen" value="Anzeigen">
+				</fieldset>
+			</form>
+			<?php
 			$kalender = new kalender();
 			
-			if (isset($_POST['jahr']) && isset($_POST['monat']))
+			if (isset($_POST['anzeigen']))
 			{
-			$kalender->baueKalender($_Post['jahr'], $_POST['monat']);
+				$kalender->baueKalender($_POST['schuljahr']);
 			}
-			else 
+			else
 			{
-				$kalender->baueKalender(date("Y"), date("m"));
+				$kalender->baueKalender(date("Y", time()));
 			}
 			?>
 			 </main>
-			
+
 		</div>
 		
 		<?php
-		include_once $wurzelVerzeichnis.'/html_include/footer.php';
+		include_once $wurzelVerzeichnis . '/html_include/footer.php';
 		?>
 
 	</div>
