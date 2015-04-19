1977ef39f6775fbfbed39bdad0f2890f52eaf857
dt <daniel.thielking@gmail.com>
2015-04-19 18:49:29 +0200

    Passwort änderungs Formular eingefügt.

--- admin/lehrer_anlegen.php
+++ admin/lehrer_anlegen.php
@@ -14,6 +14,37 @@ $datenbank = new mysqli($database_conf['host'], $database_conf['user'], $databas
 $datenbank->set_charset('utf8');
 
 // Überprüfung ob der Submitbutton gedrückt wurde
+// Zuständig für das Ändern des Passwortes
+// in der Lehrer Tabelle
+if (isset($_POST['aendern']))
+{
+	// Überprüfung ob alle Felder des Einfüge Formulars ausgefüllt wurden
+	if ($_POST['pwd'] != "")
+	{
+		// Erstellen der Einfüge anweisung in SQL
+		$insertquery = "UPDATE lehrer ";
+		$insertquery .= "SET passwort = '";
+		$insertquery .= verschluesselLogin($_POST['pwd']);
+		$insertquery .= "' WHERE lehrerID = '" . $_POST['benutzername'] . "'";
+		
+		// Einfügen der Formulardaten in die Lehrertabelle
+		$datenbank->query($insertquery);
+		
+		// Überprüfung ob der Datensatz angelegt wurde
+		if ($datenbank->affected_rows > 0)
+		{
+			// Speichern des Ausgabestrings in eine Variable
+			$ausgabe = "<hr><p class=\"erfolgreich\">Es wurde 1 Datensatz geaendert.</p>";
+		}
+	}
+	else
+	{
+		// Speichern des Fehlerstrings in eine Variable
+		$ausgabe = "<hr><p class=\"error\">Alle Felder müssen ausgefüllt werden!</p>";
+	}
+}
+
+// Überprüfung ob der Submitbutton gedrückt wurde
 // Zuständig für das Einfügen eines neuen Lehrers
 // in die Lehrer Tabelle
 if (isset($_POST['benanlegen']))
@@ -66,13 +97,16 @@ if (isset($_POST['loeschelehrer']) && isset($_POST['loesche']))
 
 // Einfache Abfragen für das Extrahieren der Abteilungen und der Lehrer
 $abfrageabteilung = "select * from abteilung;";
+$abfrageBenutzer = "select * from lehrer;";
 $abfragelehrer = "select l.lehrerID, l.vorname, l.nachname, l.kuerzel, l.benutzername, l.administrator, a.name ";
 $abfragelehrer .= "from lehrer l, abteilung a ";
 $abfragelehrer .= "where l.abteilungID = a.abteilungID";
+;
 
 // Ergebnis der Abfrage aus $abfragelehrer
 $ergabfragerlehrer = $datenbank->query($abfragelehrer);
 ?>
+
 <form class="anlegen" action="<?php $_SERVER['PHP_SELF']?>" method="post" name="lehrereinfuegen" class="lehrereinfuegen">
 	<fieldset>
 		<legend>Einfügen des Lehrpersonals in das 'CTS'</legend>
@@ -113,6 +147,28 @@ $ergabfragerlehrer = $datenbank->query($abfragelehrer);
 	</fieldset>
 </form>
 
+<form class="anlegen" action="<?php $_SERVER['PHP_SELF']?>" method="post" name="passwortaendern" class="passwortaendern">
+	<fieldset>
+		<legend>Passwort ändern des Lehrpersonals</legend>
+		<p>
+			<label for="benutzername">Benutzername:</label> <select id="benutzername" name="benutzername">
+							<?php
+							$ergebnisBenutzer = $datenbank->query($abfrageBenutzer);
+							while ($daten = $ergebnisBenutzer->fetch_object())
+							{
+								echo "<option value=\"$daten->lehrerID\">" . $daten->vorname . " " . $daten->nachname . "</option>";
+							}
+							?>
+						</select>
+		</p>
+		<p>
+			<label for="passwort">Passwort:</label> <input type="password" min="5" name="pwd">
+		</p>
+		<p class="button">
+			<label>&nbsp;</label><input type="submit" name="aendern" value="Passwort ändern"> <input type="reset" name="reset" value="Zurücksetzen">
+		</p>
+	</fieldset>
+</form>
 
 <?php
 // Ausgabe ob eintrag in die Datenbank erfolgreich war.
