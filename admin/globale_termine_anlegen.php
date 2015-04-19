1042502454b81512caad8af4f7c1c0b2255f91dc
dt <daniel.thielking@gmail.com>
2015-04-19 22:00:47 +0200

    Eingefügt

--- admin/globale_termine_anlegen.php
+++ admin/globale_termine_anlegen.php
@@ -5,55 +5,144 @@ $pruefeSession->AdminBereich();
 
 ?>
 
-<form action="<?php $_SERVER['PHP_SELF']?>" method="post" name="globalenterminanlegen" class="anlegen">
+<?php
+
+// Datenbankverbindung initialisieren
+$datenbank = new mysqli($database_conf['host'], $database_conf['user'], $database_conf['password'], $database_conf['database']);
+$datenbank->set_charset('utf8');
+
+if (isset($_POST['anlegen']))
+{
+	
+	if (!isset($_POST['ganzertag'][0]))
+	{
+		$_POST['ganzertag'][0] = 0;
+	}
+	
+	if ($_POST['nametermin'] != "" && $_POST['beginndatum'] != "" && $_POST['endedatum'] != "")
+	{
+		$insertquery = "INSERT INTO belegtetage (name, beginndatum, endedatum, ganzertag) values ";
+		$insertquery .= "('" . $_POST['nametermin'] . "', '" . $_POST['beginndatum'] . "', '" . $_POST['endedatum'];
+		$insertquery .= "', '" . $_POST['ganzertag'][0] . "');";
+		
+		try
+		{
+			$datenbank->query($insertquery);
+		}
+		catch (Exception $e)
+		{
+			echo $e->getMessage();
+		}
+		
+		if ($datenbank->affected_rows > 0)
+		{
+			$ausgabe = "<hr><p class=\"erfolgreich\">Es wurde 1 Datensatz angelegt.</p>";
+		}
+	}
+	else
+	{
+		// Speichern des Fehlerstrings in eine Variable
+		$ausgabe = "<hr><p class=\"error\">Alle Felder müssen ausgefüllt werden!</p>";
+	}
+}
+
+if (isset($_POST['loeschetermin']) && isset($_POST['loesche']))
+{
+	$terminDelete = "DELETE from belegtetage where belegtID = " . $_POST['loesche'];
+	
+	$datenbank->query($terminDelete);
+	
+	if ($datenbank->affected_rows > 0)
+	{
+		$ausgabe = "<hr><p class=\"erfolgreich\">Es wurde der Datensatz mit der ID: " . $_POST['loesche'] . " gelöscht.</p>";
+	}
+	else
+	{
+		// Wenn die Abteilung nicht gelöscht wurde
+		// Speichern der Error meldung in die Variable
+		$ausgabe = "<hr><p class=\"error\">Fehler! es wurde kein Datensatz gelöscht</p>";
+	}
+}
+
+// Abfragen
+$terminQuery = "SELECT * from belegtetage;";
+
+// Durchgeführte Abfrage
+$terminErgebnis = $datenbank->query($terminQuery);
+?>
+
+<form action="<?php $_SERVER['PHP_SELF']?>" method="post" name="globaletermineanlegen" class="anlegen">
 	<fieldset>
-		<legend>Globalen Termin Anlegen</legend>
+		<legend>Globale Termine anlegen</legend>
 		<p>
-			<label for="datum">Datum</label><input type="date" min="<?php echo date("Y-m-d", time()) ?>" id="datum" name="datum" value="<?php  echo date("Y-m-d", time()) ?>"/>
+			<label for="nametermin">Terminname:</label><input type="text" id="nametermin" name="nametermin" />
 		</p>
 		<p>
-			<label for="art">Art</label><select id="art" name="art">
-				<option contenteditable="true" value="" selected="selected">Auswahl treffen</option>
-				<option value="1">Klausur</option>
-				<option value="0">Test</option>
-			</select>
+			<label for="beginndatum">Terminbeginn:</label><input type="date" min="<?php echo date("Y-m-d", time())?>" id="beginndatum" name="beginndatum" value="<?php echo date("Y-m-d", time())?>" />
 		</p>
 		<p>
-			<label for="thema">Tema</label><input type="text" id="thema" name="thema">
-	 	</p>
-	 	<p>
-	 		<label for="vonstunde">Begin</label><select id="vonstunde" name="vonstunde">
-	 		<?php 
-	 			for ($i = 1 ; $i <= 12 ; $i++)
-	 			{
-	 				if ($i < 10)
-	 				{
-	 					echo "<option value=\"$i\">Anfang 0$i. Stunde</option>";
-	 				}
-	 				else
-	 				{
-	 					echo "<option value=\"$i\">Anfang $i. Stunde</option>";
-	 				}
-	 			}
-	 		?>
-	 		</select>
-	 	</p>
-	 	<p>
-	 		<label for="bisstunde">Ende</label><select id="bisstunde" name="bisstunde">
-	 		<?php 
-	 			for ($i = 1 ; $i <= 12 ; $i++)
-	 			{
-	 				if ($i < 10)
-	 				{
-	 					echo "<option value=\"$i\">Ende 0$i. Stunde</option>";
-	 				}
-	 				else
-	 				{
-	 					echo "<option value=\"$i\">Ende $i. Stunde</option>";
-	 				}
-	 			}
-	 		?>
-	 		</select>
-	 	</p>
+			<label for="endedatum">Terminende:</label><input type="date" min="<?php echo date("Y-m-d", time())?>" id="endedatum" name="endedatum" value="<?php echo date("Y-m-d", time())?>" />
+		</p>
+		<p>
+			<label for="ganzertag">Ganzer Tag:</label><input type="checkbox" id="ganzertag" name="ganzertag[]" value="1" />
+		</p>
+		<p>
+			<label>&nbsp;</label><input type="submit" name="anlegen" value="Termin anlegen"> <input type="reset" value="Zurücksetzen">
+	
 	</fieldset>
 </form>
+<?php
+if (isset($ausgabe))
+{
+	echo $ausgabe;
+}
+?>
+<hr>
+<table class="ausgabe">
+	<tr>
+		<th>TerminID</th>
+		<th>Terminname</th>
+		<th>Von/Bis Datum</th>
+		<th>Ganzer Tag</th>
+		<th><form action="<?php $_SERVER['PHP_SELF'] ?>" method="post">
+				<input type="submit" value="Löschen" name="loeschetermin" /></th>
+	</tr>
+				<?php
+				while ($daten = $terminErgebnis->fetch_object())
+				{
+					echo "<tr>";
+					echo "<td>" . $daten->belegtID . "</td>";
+					echo "<td>" . $daten->name . "</td>";
+					if ($daten->beginndatum == $daten->endedatum)
+					{
+						echo "<td>" . date("d.m.Y", strtotime($daten->beginndatum)) . "</td>";
+					}
+					else
+					{
+						echo "<td>" . date("d.m.", strtotime($daten->beginndatum)) . "-" . date("d.m.Y", strtotime($daten->endedatum)) . "</td>";
+					}
+					if ($daten->ganzertag == 1)
+					{
+						echo "<td>Ja</td>";
+					}
+					else
+					{
+						echo "<td>Nein</td>";
+					}
+					echo "<td><input type=\"radio\" value=\"$daten->belegtID\" name=\"loesche\" />";
+					echo "</tr>";
+				}
+				?>
+				<tr>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+		<td>&nbsp;</td>
+		<td><input type="submit" value="Löschen" name="loeschetermin" />
+			</form></td>
+
+</table>
+
+<?php
+$datenbank->close();
+?>
