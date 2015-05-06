2a1c2279c55eb8c08aea050913975737be961867
Luzifer2222 <luzifer2222@gmail.com>
2015-05-06 14:27:16 +0200

    ...

--- klassen/stundenplan_anzeigen.php
+++ klassen/stundenplan_anzeigen.php
@@ -35,105 +35,10 @@ $fachErgebnis = $datenbank->query($fachQuery);
 		?>
 		</select>
 		<input type="submit" name="anzeigen" value="Anzeigen">
-		<input type="submit" name="aendern" value="Ändern">
-		<?php if (isset($_POST['aendern'])): ?>
-		<input type="submit" name="speichern" value="Speichern">
-		<?php endif ?>
 		</p>
 	</fieldset>
 </form>
-
-<?php
-// Überprüfung ob der Submitbutton gedrückt wurde
-// Zuständig für das Ändern des Stundenplans
-if (isset($_POST['aendern']))
-{
-	echo "<hr>";
-	echo "<form action=\"\" method=\"post\" name=\"tabelle\" class=\"formular\">";
-	echo "<table class=\"kalender\" id=\"stundenplan\">";
-		
-	// Abfrage des Namens der ausgewählten Klasse aus der Tabelle Klassen
-	$classQuery = "SELECT name FROM klassen Where klassenID = " . $_POST['klassenwahl'] . ";";
-	$classErgebnis = $datenbank->query($classQuery);
-	while ($daten = $classErgebnis->fetch_object())
-	{
-		$klassenname = $daten->name;
-	}
-	
-	// Ausgabe des Stundenplans zu der ausgewählten Klasse
-	echo "<caption>Klasse " . $klassenname . "</caption>";
-	echo "<tr>";
-	echo "<th class=\"vonbis\">Stunde</th>";
-	echo "<th>Montag</th>";
-	echo "<th>Dienstag</th>";
-	echo "<th>Mittwoch</th>";
-	echo "<th>Donnerstag</th>";
-	echo "<th>Freitag</th>";
-	echo "</tr>\n";
-	// Abfrage der Stunden der gewählten Klasse aus der Tabelle Stunden
-	$sQuery = "Select klassenID, name from stunden ";
-	$sQuery .= "where klassenID = '" . $_POST['klassenwahl'] . "';";
-	$sErgebnis = $datenbank->query($sQuery);
-	
-	// Hochzählender Eintrag zur Abfrage der einzelnen Felder
-	$wert = 1;
-	$eintrag = "feld";
-	
-	// Erste bis achte Schulstunde, falls es mehr gibt, muss dieser erhöht werden
-	for ($schulstunde = 1 ; $schulstunde < 9 ; $schulstunde++)
-	{
-		echo "<tr>";
-		echo "<td>$schulstunde</td>\n";
-		
-		// Montag bis Freitag, falls es Samstags Unterricht gibt, muss dieser Wert erhöht werden
-		for ($tag = 1 ; $tag < 6 ; $tag++)
-		{
-			
-			// Einfache Abfrage
-			$fragestunde = "select f.name ";
-			$fragestunde .= "from faecher f, stunden s ";
-			$fragestunde .= "where s.klassenID = " . $_POST['klassenwahl'] . " AND s.wochentagID = $tag AND s.stunde = $schulstunde AND f.fachID = s.fachID;";
-			
-			// Ergebnis der Abfrage aus $fragestunde
-			$ergfragestunde = $datenbank->query($fragestunde);
-			$pruefe = NULL;
-			$boolean = false;
-			// Benennung der Felder durch den hochzählenden Eintrag zur Abfrage der einzelnen Felder
-			$feld = "$eintrag$wert";
-			while ($daten = $ergfragestunde->fetch_object())
-			{
-				$pruefe = $daten->name;
-			}
-			echo "<td><select class=\"plan\" name=\"$feld\">";
-				$fachErgebnis = $datenbank->query($fachQuery);
-				while ($daten = $fachErgebnis->fetch_object())
-				{
-					echo "<option value=\"$daten->fachID\" ";
-					if ($daten->name == $pruefe)
-					{
-						echo "selected=\"selected\"";
-						$boolean = true;
-					}
-					echo " >" . $daten->name . "</option>";
-				}
-				if ($boolean == false)
-				{
-					echo "<option value=\"0\" selected=\"selected\">&nbsp;</option>";
-				}
-				else
-				{
-					echo "<option value=\"0\">&nbsp;</option>";
-				}
-			echo "</select></td>\n";
-			// Hochzählender Eintrag zur Abfrage der einzelnen Felder
-			$wert++;
-		}
-	echo "</tr>";
-	}
-	echo "</table>";
-	echo "</form>;"
-}
-?>
+<hr>
 
 <?php
 // Überprüfung ob der Submitbutton gedrückt wurde
@@ -141,19 +46,25 @@ if (isset($_POST['aendern']))
 // aus der Tabelle Stunden
 if (isset($_POST['anzeigen']))
 {	
-	echo "<hr>";
 	echo "<table class=\"kalender\" id=\"stundenplan\">";
 	
-	// Abfrage des Namens der ausgewählten Klasse aus der Tabelle Klassen
+	// Abfrage der Namen der ausgewählten Klasse und dem zugehörigem Klassenlehrer aus der Tabelle Klassen
 	$classQuery = "SELECT name FROM klassen Where klassenID = " . $_POST['klassenwahl'] . ";";
 	$classErgebnis = $datenbank->query($classQuery);
 	while ($daten = $classErgebnis->fetch_object())
 	{
 		$klassenname = $daten->name;
 	}
+	$teacherQuery = "SELECT l.nachname, l.vorname FROM klassen k, lehrer l Where klassenID = " . $_POST['klassenwahl'] . " AND k.klassenlehrerID = l.lehrerID;";
+	$teacherErgebnis = $datenbank->query($teacherQuery);
+	while ($daten = $teacherErgebnis->fetch_object())
+	{
+		$lehrervorname = $daten->vorname;
+		$lehrernachname  = $daten->nachname;
+	}
 	
 	// Ausgabe des Stundenplans zu der ausgewählten Klasse
-	echo "<caption>Klasse " . $klassenname . "</caption>";
+	echo "<caption>Klasse: " . $klassenname . " Klassenlehrer: " . $lehrervorname . " " . $lehrernachname . "</caption>";
 	echo "<tr>";
 	echo "<th class=\"vonbis\">Stunde</th>";
 	echo "<th>Montag</th>";
@@ -181,7 +92,7 @@ if (isset($_POST['anzeigen']))
 			// Einfache Abfrage
 			$fragestunde = "select stunde ";
 			$fragestunde .= "from stunden ";
-			$fragestunde .= "where klassenID = " . $_POST['klassenwahl'] . " AND wochentagID = $tag AND stunde = $schulstunde;";
+			$fragestunde .= "where klassenID = " . $_POST['klassenwahl'] . " AND wochentag = $tag AND stunde = $schulstunde;";
 			
 			// Ergebnis der Abfrage aus $fragestunde
 			$ergfragestunde = $datenbank->query($fragestunde);
@@ -196,7 +107,7 @@ if (isset($_POST['anzeigen']))
 				// Einfache Abfrage
 				$fQuery = "Select f.name ";
 				$fQuery .= "from faecher f, stunden s ";
-				$fQuery .= "where s.klassenID = " . $_POST['klassenwahl'] . " AND s.wochentagID = $tag AND s.stunde = $schulstunde AND f.fachID = s.fachID;";
+				$fQuery .= "where s.klassenID = " . $_POST['klassenwahl'] . " AND s.wochentag = $tag AND s.stunde = $schulstunde AND f.fachID = s.fachID;";
 				
 				// Ergebnis der Abfrage aus $fQuery
 				$fachErg = $datenbank->query($fQuery);
@@ -213,42 +124,10 @@ if (isset($_POST['anzeigen']))
 		echo "</tr>";
 	}
 	echo "</table>";
+	echo "<hr>";
 }
 ?>
 
-<?php
-// Überprüfung ob der Submitbutton gedrückt wurde
-// Zuständig für das Speichern des Stundenplans
-// in die Tabelle Stunden
-
-if (isset($_POST['speichern']))
-{
-	// Hochzählender Eintrag zur Abfrage der einzelnen Felder
-	$wert = 1;
-	$eintrag = "feld";
-	for ($schulstunde = 1 ; $schulstunde < 9 ; $schulstunde++)
-	{
-		for ($tag = 1 ; $tag < 6 ; $tag++)
-		{
-			$feld = "$eintrag$wert";
-			if (($_POST["$feld"]) == 0)
-			{
-				
-			}
-			$wert++;
-		}
-	}
-		
-	if (($_POST['feld1']) == 2)
-	{
-		echo "test2";
-	}
-	if (($_POST['feld1']) == 1)
-	{
-		echo "test1";
-	}
-}
-?>
 
 <?php
 // Schließen der Datenbank am Ende der Seite
